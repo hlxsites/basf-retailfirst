8d2c6f183d0fe3421af8801e937d7766770d63eb
Julien Ramboz <ramboz@adobe.com>
2023-02-01 12:06:23 -0800

    feat: auto-block hero and style sections in alternate background

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,13 +17,11 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
 window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
 
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+  const section = main.querySelector('div');
+  const picture = section.querySelector('picture');
+  const elements = [...section.children];
+  if (section && picture) {
+    section.append(buildBlock('hero', { elems: elements }));
   }
 }
 
