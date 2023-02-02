0f0de1f387c659f6bd7632f4ebba693ebc817671
Julien Ramboz <ramboz@adobe.com>
2023-02-02 11:18:38 -0800

    feat: add over for cards hover effect and handle global clicks

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,11 +6,21 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
+    if (!li.querySelector('ul')) {
+      li.classList.add('has-overlay');
+    }
     [...li.children].forEach((div) => {
       [...div.childNodes].forEach((el) => {
         li.append(el);
         div.remove();
       });
+    });
+    li.addEventListener('click', (ev) => {
+      const parent = ev.target.closest('li');
+      if (!parent.classList.contains('has-overlay')) {
+        return;
+      }
+      parent.querySelector('a').click();
     });
     ul.append(li);
   });
