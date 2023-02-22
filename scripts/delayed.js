c7209f7c96f02f7f4bee1451f96bb529e036b023
Ravi Kiran Papani <rpapani@adobe.com>
2023-02-22 10:31:04 -0800

    feature: adding utag and jquery to the page in delayed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,15 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+function loadUTag() {
+  const jquery = document.createElement('script')
+  jquery.src = 'https://code.jquery.com/jquery-3.6.3.min.js';
+  jquery.async = true;
+  document.head.appendChild(jquery);
+  const utag = document.createElement('script');
+  utag.src = 'https://tags.tiqcdn.com/utag/basf/us-crop/prod/utag.js';
+  utag.async = true;
+  jquery.onload = () => document.head.appendChild(utag);
+}
+
+loadUTag();
